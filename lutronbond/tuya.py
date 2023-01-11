0777c5b281abafe5a631d77005fa5c008d1c33a5
Heston Liebowitz <heston@junelife.com>
2023-01-11 10:28:24 -0700

    Run tuya requests in separate thread

--- lutronbond/tuya.py
+++ lutronbond/tuya.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import typing
 
@@ -18,22 +19,22 @@ def get_handler(
         configmap: dict
 ) -> typing.Callable[[lutron.LutronEvent], typing.Awaitable[bool]]:
 
+    tuya_config = configmap['tuya']
+
+    try:
+        device = tinytuya.OutletDevice(
+            dev_id=tuya_config['id'],
+            address=tuya_config['addr'],
+            local_key=tuya_config['localKey'],
+            version=tuya_config['version']
+        )
+    except KeyError:
+        logger.error('Invalid Tuya device: %s', tuya_config)
+        raise
+
+    actions = tuya_config['actions']
+
     async def handler(event: lutron.LutronEvent) -> bool:
-        tuya_config = configmap['tuya']
-
-        try:
-            device = tinytuya.OutletDevice(
-                dev_id=tuya_config['id'],
-                address=tuya_config['addr'],
-                local_key=tuya_config['localKey'],
-                version=tuya_config['version']
-            )
-        except KeyError:
-            logger.error('Invalid Tuya device: %s', tuya_config)
-            return False
-
-        actions = tuya_config['actions']
-
         try:
             component = actions[event.component.name]
         except KeyError:
@@ -52,22 +53,27 @@ def get_handler(
         try:
             method_name = ACTIONS[action]
         except KeyError:
-            logger.warning('Unknown action: %s', action)
+            logger.warning('Unknown device method: %s', action)
             return False
 
         method = getattr(device, method_name)
+
+        def do_action() -> bool:
+            method()
+            logger.info(
+                '%s request sent to Tuya device %s (%s)',
+                action,
+                tuya_config['id'],
+                configmap.get('name', 'Unnamed')
+            )
+
+            return True
+
         logger.debug(
             'Starting %s request to Tuya device %s',
             action,
             tuya_config['id']
         )
-        method()
-        logger.info(
-            '%s request sent to Tuya device %s (%s)',
-            action,
-            tuya_config['id'],
-            configmap.get('name', 'Unnamed')
-        )
-        return True
+        return await asyncio.to_thread(do_action)
 
     return handler
